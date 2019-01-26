697c9b17772d446d966beb24ccc1b5ec75cd5b95
popwandee <popwandee@gmail.com>
2019-01-26 22:09:22 +0700

    Update callback.php

--- callback.php
+++ callback.php
@@ -5,7 +5,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-require __DIR__."/vendor/autoload.php";
+//require __DIR__."/vendor/autoload.php";
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 use Monolog\Handler\FirePHPHandler;
